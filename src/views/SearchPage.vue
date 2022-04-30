5667322985994b204c12ed2ee08130582f53ef94
Sebastian Villafana <sebvs@hey.com>
2022-05-01 00:24:12 +1000

    M2: update class names in SearchPage

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -4,16 +4,16 @@
       Search for a manual
     </label>
     <input
+      v-model="searchInput"
+      class="w-7/8 block h-12 rounded-2xl border-4 px-4 py-8 font-mono text-2xl caret-black"
       name="searchField"
       type="text"
-      v-model="searchInput"
-      class="w-7/8 block h-12 rounded-2xl border-4 px-4 py-8 font-serif font-mono text-2xl caret-black"
-      placeholder="Search for a manual"
+      placeholder="Enter text"
       aria-label="search input"
     />
     <button
-      aria-label="submit search"
       class="rounded-2xl border-4 border-white bg-black p-4 font-mono text-2xl text-white hover:border-4 hover:border-black hover:bg-white hover:text-black"
+      aria-label="submit search"
       @click="submitSearch(searchInput)"
     >
       Submit search
