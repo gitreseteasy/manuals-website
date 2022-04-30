0d5b8b97f9a8dffe5016f66491b417998eb6ce79
Sebastian Villafana <sebvs@hey.com>
2022-04-30 21:44:38 +1000

    M-2: Create basic front page with stubbed submit method

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav>
-<!--    <router-link to="/"></router-link>-->
-  </nav>
-  <router-view />
-  Ready to go
+  <div class="flex h-full flex-col justify-start">
+    <nav class="border-b-4 border-black p-4 hover:font-extrabold">
+      <router-link class="font-mono text-2xl italic" to="/">Home</router-link>
+    </nav>
+    <router-view class="flex-grow p-4" />
+  </div>
 </template>
---
+++ src/views/SearchPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-16 flex w-full flex-col items-center justify-center space-y-4">
+    <label for="searchField" class="block font-mono text-3xl font-extralight">
+      Search for a manual
+    </label>
+    <input
+      name="searchField"
+      type="text"
+      v-model="searchInput"
+      class="w-7/8 block h-12 rounded-2xl border-4 px-4 py-8 font-serif font-mono text-2xl caret-black"
+      placeholder="Search for a manual"
+      aria-label="search input"
+    />
+    <button
+      aria-label="submit search"
+      class="rounded-2xl border-4 border-white bg-black p-4 font-mono text-2xl text-white hover:border-4 hover:border-black hover:bg-white hover:text-black"
+      @click="submitSearch(searchInput)"
+    >
+      Submit search
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class SearchPage extends Vue {
+  searchInput = '';
+
+  submitSearch(input: string) {
+    console.log(`submit search for ${input}`);
+  }
+}
+</script>
