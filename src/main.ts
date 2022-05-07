e01c771194a2b4a991e653c4d089e2338f033a19
Sebastian Villafana <sebvs@hey.com>
2022-05-07 20:33:26 +1000

    M-7: Add date-fns and replace vuex with pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-import store from './store';
+import {createPinia as store} from 'pinia';
 import './index.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
